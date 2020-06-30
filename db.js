a49c90bc05f699640a7e64564e671b8fbaaf742d
awanaprilino <aprilinoawan@gmail.com>
2020-06-30 15:18:18 +0700

    Second

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "awanpc",
-  database: "todo_database",
-  host: "localhost",
+  host: "ec2-34-239-241-25.compute-1.amazonaws.com",
+  database: "dbem69s6ah1k6r",
+  user: "isdwfhjgezpgep",
   port: 5432,
+  password: "031c65a450fe207355829c48895129ff6e97db761655ade0fa050ff15e4054bb",
 });
 
 module.exports = pool;
